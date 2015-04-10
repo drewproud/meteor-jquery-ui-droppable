27ee1899a9e4a02d86d4d84e97ac509b930e3a07
Daniel Bernhard <me@danielbernhard.com>
2015-04-10 18:13:17 -0400

    update pacakge

--- package.js
+++ package.js
@@ -1,9 +1,12 @@
 Package.describe({
   summary: "Enables dragging functionality for any element",
-  git: "https://github.com/dbernhard/meteor-jquery-ui-draggable.git"
+  git: "https://github.com/dbernhard/meteor-jquery-ui-draggable.git",
+  version: "0.1.0",
+  name: "dbernhard:jquery-ui-draggable"
 });
 
 Package.on_use(function(api) {
+  api.versionsFrom('METEOR@1.1.0.2');
   api.use('jquery', 'client');
   api.add_files(['jquery-ui-draggable.js', 'jquery-ui-draggable.css'], 'client');
 });
